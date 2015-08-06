ae03bfbf0061607106c0aa47cb6f04b55bbaa2f8
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-08-06 12:43:53 +0200

    re-merged the devices subpackage into ratcave, in order to support calibration console scripts.

--- build/lib/ratcave/__init__.py
+++ build/lib/ratcave/__init__.py
@@ -1,11 +1,4 @@
 __author__ = 'ratcave'
 
-import transformations
-import resources
-from __camera import Camera, projector, projector_position
-from __mesh import Mesh
-from __scene import Scene
-from __window import Window
-from __wavefront import WavefrontReader
-
-
+import graphics
+import devices
