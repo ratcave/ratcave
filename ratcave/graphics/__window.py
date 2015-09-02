f95279854e530eafa70be4d4eedbbf0b015206b4
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-09-02 19:37:21 +0200

    moved all render_x Scene methods to window, and started removing class attributes.

--- ratcave/graphics/__window.py
+++ ratcave/graphics/__window.py
@@ -1,28 +1,113 @@
 __author__ = 'nickdg'
 
 from psychopy import visual
-
+import pyglet.gl as gl
 from __scene import Scene
+from __camera import Camera
+from __shader import Shader
+from utils import *
+from collections import namedtuple
 
+FBO = namedtuple('FBO', 'id texture')
+shader_path = join(split(__file__)[0], 'shaders')
 
 class Window(visual.Window):
     """Subclass of Pyglet window, with some defaults set to simplify ratCAVE script creation."""
 
-    def __init__(self, active_scene, *args, **kwargs):
-        kwargs['winType'] = 'pyglet'
+    # General, Normal Shader
+    genShader = Shader(open(join(shader_path, 'combShader.vert')).read(),
+                    open(join(shader_path, 'combShader.frag')).read())
+
+    shadowShader = Shader(open(join(shader_path, 'shadowShader.vert')).read(),
+                        open(join(shader_path, 'shadowShader.frag')).read())
+
+    aaShader = Shader(open(join(shader_path, 'antialiasShader.vert')).read(),
+                        open(join(shader_path, 'antialiasShader.frag')).read())
+
+    def __init__(self, active_scene, grayscale=False, *args, **kwargs):
         kwargs['allowStencil'] = False
 
         super(Window, self).__init__(*args, **kwargs)
+        assert self.winType == 'pyglet', "Window Type must be 'pyglet' for ratCAVE to work."
 
         self.active_scene = active_scene  # For normal rendering.
         self.virtual_scene = None  # For dynamic cubemapping.
+        self.grayscale = grayscale
+        self.fbos = {'shadow': FBO(create_fbo(gl.GL_TEXTURE_2D, 2048, 2048, texture_slot=5, color=False, depth=True)),
+                     'cube': FBO(create_fbo(gl.GL_TEXTURE_CUBE_MAP, 2048, 2048, texture_slot=0, color=True, depth=True, grayscale=self.grayscale)),
+                     'antialias': FBO(create_fbo(gl.GL_TEXTURE_2D, 1280, 720, texture_slot=0, color=True, depth=True, grayscale=self.grayscale))
+                     }
+        self.texture_size = 2048
+        self.player = None
 
     def set_virtual_scene(self, scene, from_viewpoint, to_mesh):
         """Set scene to render to cubemap, as well as the object whose position will be used as viewpoint and what mesh
         will be given the cubemap texture."""
-        self.virtual_scene = scene
-        Scene.player = from_viewpoint
+        self.virtual_scene, self.player = scene, from_viewpoint
         to_mesh.cubemap = True
+
+    def render_shadow(self, scene):
+
+        # render to Frame Buffer Object (FBO) (depth values only)
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, self.fbos['shadow'].id)  # Rendering off-screen
+        gl.glViewport(0, 0, self.texture_size , self.texture_size )
+
+        # Render Scene
+        scene._draw(Camera(fov_y=60., aspect=1., position=scene.light.position, rotation=scene.camera.rotation), Window.shadowShader)
+
+        # Reset Render settings to normal and unbind FBO
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, 0)
+        gl.glViewport(0, 0, self.size[0], self.size[1])
+
+    def render_to_cubemap(self, scene):
+        """
+        Renders the scene, 360 degrees around a camera, to a mesh "toMesh" from the perspective of "fromObject".
+        Currently, window width and height must also be supplied.
+        """
+
+        # Bind to Cubemap Framebuffer to render directly to the cubemap texture.
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, self.fbos['cube'].id)
+        gl.glViewport(0, 0, self.texture_size, self.texture_size )  # Change viewport to square texture, as big as possible.
+
+
+        # Render to Each Face of Cubemap texture, rotating the camera in the correct direction before each shot.
+        cube_camera = Camera(fov_y=90., aspect=1., position=self.player.position)
+        for face, rotation in enumerate([[180, 90, 0], [180, -90, 0], [90, 0, 0], [-90, 0, 0], [180, 0, 0], [0, 0, 180]]):  # Created as class variable for performance reasons.
+            Window.__cubeCamera.rotation = rotation
+            gl.glFramebufferTexture2DEXT(gl.GL_FRAMEBUFFER_EXT, gl.GL_COLOR_ATTACHMENT0_EXT,
+                                         gl.GL_TEXTURE_CUBE_MAP_POSITIVE_X + face,
+                                         self.fbos['cube'].texture,  0)  # Select face of cube texture to render to.
+            self._draw(Window.__cubeCamera, Scene.genShader)  # Render
+
+        # Restore previous camera position and lens settings
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, 0)  # Unbind cubemap Framebuffer (so rendering to screen can be done)
+        gl.glViewport(0, 0, self.size[0], self.size[1])  # Reset Viewport
+
+    def render_to_antialias(self):
+
+        # Bind to Cubemap Framebuffer to render directly to the cubemap texture.
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, self.fbos['antialias'].id)
+        self._draw(self.camera, Scene.genShader)
+        gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, 0)  # Unbind cubemap Framebuffer (so rendering to screen can be done)
+
+        # Render Scene onto a fullscreen quad, after antialiasing.
+        gl.glViewport(*Scene._viewport)  # Reset Viewport
+        self._render_to_fullscreen_quad(Scene.aaShader, Scene._aaTexture)
+
+
+
+    def _render_to_fullscreen_quad(self, shader, texture):
+        """Fairly general method, to be converted to more general deferred shading rendering method."""
+        gl.glClearColor(self.bgColor.r, self.bgColor.g, self.bgColor.b, 1.)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        shader.bind()
+        shader.uniformf('frameBufSize', *self.size)
+        shader.uniformi('image_texture', 0)
+        shader.uniformi('grayscale', int(Scene.grayscale))
+        gl.glBindTexture(gl.GL_TEXTURE_2D, texture)
+        fullscreen_quad.render()
+        shader.unbind()
+        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
 
     def on_draw(self):
         """Active scene drawn, virtual scene is rendered to a cubemap."""
--- ratcave/graphics/utils.py
+++ ratcave/graphics/utils.py
@@ -2,6 +2,79 @@ import pyglet.gl as gl
 import numpy as np
 import pdb
 
+def create_opengl_object(gl_gen_function):
+    """Returns int pointing to an OpenGL texture"""
+    texture_id = gl.GLuint(1)
+    gl_gen_function(1, byref(texture_id))  # Create Empty Texture
+    return texture_id.value  # Use handle for rest
+
+
+def create_fbo(texture_type, width, height, texture_slot=0, color=True, depth=True, grayscale=False):
+
+    assert color or depth, "at least one of the two data types, color or depth, must be set to True."
+
+    # Make a texture and bind it.
+    gl.glActiveTexture(gl.GL_TEXTURE0 + texture_slot)
+    texture = create_opengl_object(gl.glGenTextures)  # Create a general texture
+    gl.glBindTexture(texture_type, texture)  # Bind it.
+
+    # Apply texture settings for interpolation behavior (Required)
+    gl.glTexParameterf(texture_type, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+    gl.glTexParameterf(texture_type, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexParameterf(texture_type, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
+    gl.glTexParameterf(texture_type, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
+    if texture_type == gl.GL_TEXTURE_CUBE_MAP:
+        gl.glTexParameterf(texture_type, gl.GL_TEXTURE_WRAP_R, gl.GL_CLAMP_TO_EDGE)
+
+    # Generate empty texture(s)
+    color_type = gl.GL_R8 if grayscale else gl.GL_RGBA
+    internal_format = gl.GL_DEPTH_COMPONENT if depth and not color else color_type
+
+    color_type = gl.GL_RED if grayscale else gl.GL_RGBA
+    pixel_format = gl.GL_DEPTH_COMPONENT if depth and not color else color_type
+
+    if texture_type == gl.GL_TEXTURE_2D:
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, internal_format, width, height, 0,
+                pixel_format, gl.GL_UNSIGNED_BYTE, 0)
+    elif texture_type == gl.GL_TEXTURE_CUBE_MAP:
+        # Generate blank textures, one for each cube face.
+        for face in range(0, 6):
+            gl.glTexImage2D(gl.GL_TEXTURE_CUBE_MAP_POSITIVE_X + face, 0, internal_format,
+                            width, height, 0, pixel_format, gl.GL_UNSIGNED_BYTE, 0)
+
+    # Create FBO and bind it.
+    fbo = create_opengl_object(gl.glGenFramebuffersEXT)
+    gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, fbo)
+
+    # Set Draw and Read locations for the FBO (mostly, turn off if not doing any color stuff)
+    if depth and not color:
+        gl.glDrawBuffer(gl.GL_NONE)  # No color in this buffer
+        gl.glReadBuffer(gl.GL_NONE)
+
+    # Bind texture to FBO.
+    attachment_point = gl.GL_DEPTH_ATTACHMENT_EXT if depth and not color else gl.GL_COLOR_ATTACHMENT0_EXT
+    attached_texture2D_type = gl.GL_TEXTURE_CUBE_MAP_POSITIVE_X if texture_type==gl.GL_TEXTURE_CUBE_MAP else gl.GL_TEXTURE_2D
+    gl.glFramebufferTexture2DEXT(gl.GL_FRAMEBUFFER_EXT, attachment_point, attached_texture2D_type, texture, 0)
+
+    # create a render buffer as our temporary depth buffer, bind it, and attach it to the fbo
+    if color and depth:
+        renderbuffer = create_opengl_object(gl.glGenRenderbuffersEXT)
+        gl.glBindRenderbufferEXT(gl.GL_RENDERBUFFER_EXT, renderbuffer)
+        gl.glRenderbufferStorageEXT(gl.GL_RENDERBUFFER_EXT, gl.GL_DEPTH_COMPONENT24, width, height)
+        gl.glFramebufferRenderbufferEXT(gl.GL_FRAMEBUFFER_EXT, gl.GL_DEPTH_ATTACHMENT_EXT, gl.GL_RENDERBUFFER_EXT,
+                                        renderbuffer)
+
+    # check FBO status (warning appears for debugging)
+    FBOstatus = gl.glCheckFramebufferStatusEXT(gl.GL_FRAMEBUFFER_EXT)
+    if FBOstatus != gl.GL_FRAMEBUFFER_COMPLETE_EXT:
+        raise BufferError("GL_FRAMEBUFFER_COMPLETE failed, CANNOT use FBO.\n{0}\n".format(FBOstatus))
+
+    #Unbind FBO and return it and its texture
+    gl.glBindFramebufferEXT(gl.GL_FRAMEBUFFER_EXT, 0)
+
+    return fbo, texture
+
+
 def vec(floatlist,newtype='float'):
 
         """ Makes GLfloat or GLuint vector containing float or uint args.
