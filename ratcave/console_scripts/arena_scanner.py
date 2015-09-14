9ff99cbe9ee50ab6289b5753c840c930c94043ce
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-09-14 18:25:07 +0200

    added grids to primitives and aded point_szie attribute to mesh.

--- ratcave/console_scripts/arena_scanner.py
+++ ratcave/console_scripts/arena_scanner.py
@@ -28,7 +28,7 @@ def scan(optitrack_ip="127.0.0.1"):
     tracker = Optitrack(client_ip=optitrack_ip)
     assert "Arena" in tracker.rigid_bodies, "Must Add Arena Rigid Body in Motive!"
     wavefront_reader = WavefrontReader(ratcave.graphics.resources.obj_primitives)
-    mesh = wavefront_reader.get_mesh('Sphere', centered=True, lighting=False, scale=.01)
+    mesh = wavefront_reader.get_mesh('Sphere', centered=True, lighting=False, scale=.005)
     mesh.material.diffuse.rgb = 1, 1, 1
     mesh.world.position=[0, 0, -1]
 
@@ -49,13 +49,17 @@ def scan(optitrack_ip="127.0.0.1"):
         window.draw()
         window.flip()
 
-        time.sleep(.006)
+        time.sleep(.016)
 
         new_position = tracker.get_unidentified_positions(1)
+
         if new_position:
 
             # Try to get Arena rigid body and a single unidentified marker
-            body = tracker.get_rigid_body('Arena')
+            try:
+                body = tracker.rigid_bodies['Arena']
+            except KeyError:
+                raise KeyError("Cannot find Arena in Motive.  Please Add a Rigid Body called 'Arena' in Movie and try again!")
 
             # If successful, add the new position to the list.
             feedback = bool(new_position and body)
@@ -81,18 +85,15 @@ def normal_nearest_neighbors(data, n_neighbors=40, min_dist_filter=.04):
     latent_all, normal_all = [], []
     for dist_array, idx_array in zip(distances, indices):
 
-        # Filter out huge distances for outliers:
-        if np.min(dist_array) > min_dist_filter:
-            continue
+        if np.min(dist_array) < min_dist_filter: # Filter out huge distances for outliers:
+            pp = PCA(n_components=3).fit(data[idx_array, :]) # Perform PCA
 
-        pp = PCA(n_components=3).fit(data[idx_array, :]) # Perform PCA
+             # Get the percent variance of each component
+            latent_all.append(pp.explained_variance_ratio_)
 
-         # Get the percent variance of each component
-        latent_all.append(pp.explained_variance_ratio_)
-
-        # Get the normal of the plane along the third component (flip if pointing in -y direction)
-        normal = pp.components_[2] if pp.components_[2][1] > 0 else -pp.components_[2]
-        normal_all.append(normal)
+            # Get the normal of the plane along the third component (flip if pointing in -y direction)
+            normal = pp.components_[2] if pp.components_[2][1] > 0 else -pp.components_[2]
+            normal_all.append(normal)
 
     # Convert to NumPy Array and return
     return np.array(normal_all), np.array(latent_all)
@@ -103,8 +104,8 @@ def meshify(data, filename):
     ## IMPORT DATA ##
     # Put values of data dictionary into numpy arrays.
     body_positions, body_rotations = np.array(data['bodyPos']), np.array(data['bodyRot'])
-    data = pd.DataFrame(data['markerPos'], columns=['x', 'y', 'z'])
-    data['mask'] = True  # Identifies which rows are used in the analysis (will be updated as filters are run)
+    points = pd.DataFrame(data['markerPos'], columns=['x', 'y', 'z'])
+    points['mask'] = True  # Identifies which rows are used in the analysis (will be updated as filters are run)
 
     # Plot raw marker data as 3D Scatterplot
     """
@@ -116,34 +117,37 @@ def meshify(data, filename):
 
     ## REMOVE OUTLIERS ##
     # Remove Obviously Bad Points according to Height
-    data['mask'] &= (-.02 < data['y']) & (data['y'] < .52)   # TODO: Automatize Height-based point filtering.
+    points['mask'] &= (-.02 < points['y']) & (points['y'] < .52)   # TODO: Automatize Height-based point filtering.
 
     # Filter out data that's not within a clear plane (mainly corners and outliers)
-    normals, explained_variances = normal_nearest_neighbors(np.array(data[['x', 'y', 'z']]))
-    data = pd.concat([data, pd.DataFrame(normals, columns=['nx', 'ny', 'nz'])], axis=1)
-    data['mask'] &= explained_variances[:,2] < .005  #
+    normals, explained_variances = normal_nearest_neighbors(np.array(points[['x', 'y', 'z']]))
+    points['mask'] &= explained_variances[:,2] < .005  #
+
+    # Add normal data to each point
+    points = pd.concat([points, pd.DataFrame(normals, columns=['nx', 'ny', 'nz'])], axis=1)
+
 
     ## CLUSTER INTO SEPARATE WALL PLANES ##
     # Label Markers by Clustering of their normals
-    data['wall'] = None
+    points['wall'] = None
     walls = pd.DataFrame(columns=['nx', 'ny', 'nz', 'x', 'y', 'z'])
     for wall in range(5):
         # Cluster from a random point, taking only points within a given distance (normal direction) from it
-        print("Making Wall {0}.  Points left to assign: {1}".format(wall, sum(data['wall'].notnull()==False)))
+        print("Making Wall {0}.  Points left to assign: {1}".format(wall, sum(points['wall'].notnull()==False)))
         pdb.set_trace()
 
-        normal_data = data.loc[data['mask'] & data['wall'].notnull()==False, ['nx', 'ny', 'nz']]
+        normal_data = points.loc[points['mask'] & points['wall'].notnull()==False, ['nx', 'ny', 'nz']]
         center_idx = np.random.choice(normal_data.index.values)
 
         wall_mask = np.sqrt(((normal_data - normal_data.ix[center_idx])**2).sum(axis=1)) < .5
-        data.loc[wall_mask, 'wall'] = wall
+        points.loc[wall_mask, 'wall'] = wall
 
         # Get Mean position and normal from the cluster of points, filtering again to get rid of any outliers (so much filtering!)
-        wall_data = data[['x', 'y', 'z']].ix[wall_mask]
+        wall_data = points[['x', 'y', 'z']].ix[wall_mask]
         rotated_data = PCA(n_components=3).fit(wall_data).transform(wall_data)[:,2]
         mean, std, threshold = np.mean(rotated_data), np.std(rotated_data), 1.5
         mask = (mean - (threshold * std) < rotated_data) * (rotated_data < (mean + (threshold *std)))
-        data.loc[wall_data.index, 'mask'] &= mask
+        points.loc[wall_data.index, 'mask'] &= mask
 
         # Get normal and offset position for each wall
         normal = PCA(n_components=3).fit(wall_data.ix[mask]).components_[2]
