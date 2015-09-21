80668107f0a6390a532271cbd2bbf820cb1a6b2a
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-09-21 15:57:21 +0200

    updated plot_3d function

--- ratcave/console_scripts/arena_scanner.py
+++ ratcave/console_scripts/arena_scanner.py
@@ -64,14 +64,27 @@ def scan(optitrack_ip="127.0.0.1", rigid_body_name='Arena', pointwidth=.06, poin
     return points, body_markers
 
 
-def plot_3d(array3d, title='', ax=None, line=False):
+def plot_3d(array3d, title='', ax=None, line=False, color=None, square_axis=False):
     """make 3D scatterplot that plots the x, y, and z columns in a dataframe. Returns figure."""
     if not ax:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
+
+    if square_axis:
+        tot_range = (array3d.max(axis=0) - array3d.min(axis=0)).max() * .55
+        mn = array3d.mean(axis=0, keepdims=True) - tot_range
+        mx = array3d.mean(axis=0, keepdims=True) + tot_range
+        limits = np.vstack([mn, mx]).transpose()
+        for coord, idx in zip('xyz', range(3)):
+            getattr(ax, 'set_{0}lim3d'.format(coord))(limits[idx])
+
     plot_fun = ax.plot if line else ax.scatter
-    plot_fun(array3d[:,0], array3d[:,2], array3d[:,1])
+    if color:
+        plot_fun(array3d[:,0], array3d[:,2], array3d[:,1], color=color)
+    else:
+        plot_fun(array3d[:,0], array3d[:,2], array3d[:,1])
     plt.title(title)
+    plt.xlabel('x'), plt.ylabel('z')
     return ax
 
 def rotate_to_var(markers):
