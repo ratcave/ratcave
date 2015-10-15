58df6808eb9310318cdaa2763662bbcefb185096
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-10-15 13:12:12 +0200

    added optional argparse paramters.  Seeing other bugs, though...

--- ratcave/console_scripts/opti_projector_rotation_calib.py
+++ ratcave/console_scripts/opti_projector_rotation_calib.py
@@ -4,10 +4,11 @@ from psychopy import event
 import ratcave
 from ratcave.graphics import *
 import numpy as np
+import argparse
 
 np.set_printoptions(precision=2, suppress=True)
 
-def display(optitrack_ip="127.0.0.1"):
+def display(optitrack_ip="127.0.0.1", calib_object_name=''):
 
     # Connect to Optitrack
     tracker = ratcave.devices.Optitrack(client_ip=optitrack_ip)
@@ -16,18 +17,25 @@ def display(optitrack_ip="127.0.0.1"):
     reader = WavefrontReader(ratcave.graphics.resources.obj_arena)
     arena = reader.get_mesh('Arena', lighting=True, centered=False)
     arena.load_texture(ratcave.graphics.resources.img_colorgrid)
+    meshes = [arena]
 
-    reader = WavefrontReader(ratcave.graphics.resources.obj_primitives)
-    cube = reader.get_mesh('Sphere', lighting=True, scale=.02, centered=True)
+    if calib_object_name:
+        reader = WavefrontReader(ratcave.graphics.resources.obj_primitives)
+        cube = reader.get_mesh('Sphere', lighting=True, scale=.02, centered=True)
+        meshes.append(cube)
 
     # Create Scene and Window
-    scene = Scene([arena, cube])
+    scene = Scene(meshes)
     scene.camera = projector
     scene.camera.fov_y = 27.25
 
     scene.light.position = scene.camera.position
 
     window = Window(scene, screen=1, fullscr=True)
+
+    # Update Everything's Position
+    arena.world.position = tracker.rigid_bodies['Arena'].position
+    arena.world.rotation = tracker.rigid_bodies['Arena'].rotation_pca_y
 
     # Print the Following every time a key is detected:
     print "Camera settings:\n -shift: {0}, {1}\n -position: {2}\n -fov_y(xz): {3}\n -rotation: {4}\n\n".format(
@@ -40,7 +48,10 @@ def display(optitrack_ip="127.0.0.1"):
         # Update Everything's Position
         arena.world.position = tracker.rigid_bodies['Arena'].position
         arena.world.rotation = tracker.rigid_bodies['Arena'].rotation_pca_y
-        cube.local.position = tracker.rigid_bodies['CalibWand'].position
+
+        # If there's another object to track, then track it.
+        if calib_object_name:
+            cube.local.position = tracker.rigid_bodies[calib_object_name].position
 
         # Re-Draw Everything
         window.draw()
@@ -60,4 +71,14 @@ def display(optitrack_ip="127.0.0.1"):
 
 
 if __name__ == '__main__':
-    display()
+
+    # Get command line inputs
+    parser = argparse.ArgumentParser(description="This displays an Optitrack Rigid Body called 'Arena' from the arena_scanner and opti_projector_calib stored coordinates.")
+    parser.add_argument('-w',
+                        action='store',
+                        dest='calib_wand_name',
+                        default='',
+                        help="If a calibration wand or something is also trackable, then give the rigid body's name and a sphere will be placed in its position")
+    args = parser.parse_args()
+
+    display(calib_object_name=args.calib_wand_name)
