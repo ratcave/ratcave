5380daf22c048cc919da5a06ff9644904872fb3f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2015-11-21 18:47:40 +0100

    scene json frame encoding now grapbbing correct data

--- ratcave/graphics/core/scene.py
+++ ratcave/graphics/core/scene.py
@@ -1,6 +1,6 @@
 from . import mixins
 from .camera import Camera
-
+import warnings
 import json
 
 def encode_obj(obj):
@@ -16,10 +16,15 @@ def encode_obj(obj):
 
 physical_keys = mixins.Physical().__dict__
 def encode_phys(obj):
+    """Only grabs the data that is expected to change frame to frame"""
     try:
         d = obj.__dict__
-        dd = {key: d[key] for key in d if key in ['light', 'camera', 'meshes', 'local', 'world']}
+        dd = {key: d[key] for key in d if key in ['light', 'camera', 'local', 'world']}
         dd.update({key: d[key] for key in d if key in physical_keys})
+        try:
+            dd.update({'meshes': obj.mesh_dict})
+        except:
+            pass
         try:
             dd['visible'] = d['visible']
         except:
@@ -38,11 +43,21 @@ class Scene(object):
 
         # Initialize List of all Meshes to draw
         self.meshes = list(meshes)
+        if len(set(mesh.data.name for mesh in self.meshes)) != len(self.meshes):
+            warnings.warn('Warning: Mesh.data.names not all unique--log data will overwrite some meshes!')
         self.camera = Camera() if not camera else camera # create a default Camera object
         self.light = mixins.Physical() if not light else light
         self.bgColor = mixins.Color(*bgColor)
 
+    @property
+    def mesh_dict(self):
+        return {mesh.data.name:mesh for mesh in self.meshes}
+
     def to_json_header(self):
         return json.dumps(self, default=encode_obj)
+
+    def to_json_tracker(self):
+        return json.dumps(self, default=encode_phys)
+
 
 
