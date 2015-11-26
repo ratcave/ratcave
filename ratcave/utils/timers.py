f076912c6aa5e670871601226e58aa7e41f55f4b
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-11-26 14:25:03 +0100

    added a dt_timer to the utils

--- ratcave/utils/timers.py
+++ ratcave/utils/timers.py
@@ -1,10 +1,10 @@
-__author__ = 'nickdg'
+import time
 
 def countdown_timer(total_secs, stop_iteration=False):
     """Generates an iterator that returns the time remaining from total_time (secs). Returns 0.0 when time is up, unless
     stop_iteration is True."""
 
-    import time
+
     end_secs = time.time() + total_secs
     while True:
         remaining_secs = end_secs - time.time()
@@ -15,3 +15,11 @@ def countdown_timer(total_secs, stop_iteration=False):
                 raise StopIteration("Time is up!")
             else:
                 yield 0.0  # Don't raise a StopIteration error, just return 0
+
+def dt_timer():
+    """A Generator that returns the time since last called. Never returns StopIteration."""
+    last_time = time.time()
+    while True:
+        dt = time.time() - last_time
+        last_time = dt
+        yield dt
