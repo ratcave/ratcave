43b2e47560ca1b28c78e0e71418e7d3163ce2a0f
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-11-28 16:44:37 +0100

    new script utility functions

--- ratcave/utils/script_utils.py
+++ ratcave/utils/script_utils.py
@@ -1,7 +1,7 @@
 __author__ = 'ratcave'
 import numpy as np
 from .. import graphics
-
+import orienting
 
 def motive_camera_vislight_configure():
     import motive
@@ -18,14 +18,19 @@ def motive_camera_vislight_configure():
                 cam.set_settings(0, cam.exposure, cam.threshold, cam.intensity)
 
 
-def correct_orientation(rb, n_attempts=3):
+def correct_orientation_motivepy(rb, n_attempts=3):
     import motive
-    import orienting
     """Reset the orientation to account for between-session arena shifts"""
     for attempt in range(n_attempts):
             rb.reset_orientation()
             motive.update()
     additional_rotation = orienting.rotate_to_var(np.array(rb.point_cloud_markers))
+    return additional_rotation
+
+def correct_orientation_natnet(rb, n_attempts=3):
+    """Assumes the orientation is reset already (need MotivePy to do it automatically) to account for between-session arena shifts"""
+    print("Warning: Assuming that the orientation has been reset to 0,0,0 for the {} rigid body".format(rb.name))
+    additional_rotation = orienting.rotate_to_var(np.array([m.position for m in rb.markers]))
     return additional_rotation
 
 
@@ -36,10 +41,18 @@ def get_arena_from(file_name=graphics.resources.obj_arena, cubemap=True):
     arena.cubemap = cubemap
     return arena
 
-def update_world_position(meshes, arena_rb, additional_rot_y_rotation):
+def update_world_position_motivepy(meshes, arena_rb, additional_rot_y_rotation):
     """# Update the positions of everything, based on the MotivePy data of the arena rigid body"""
     for mesh in meshes:
         mesh.world.position = arena_rb.location
         mesh.world.rotation = arena_rb.rotation_global
+        mesh.world.rot_y += additional_rot_y_rotation
+    return
+
+def update_world_position_natnet(meshes, arena_rb, additional_rot_y_rotation):
+    """# Update the positions of everything, based on the MotivePy data of the arena rigid body"""
+    for mesh in meshes:
+        mesh.world.position = arena_rb.position
+        mesh.world.rotation = arena_rb.rotation
         mesh.world.rot_y += additional_rot_y_rotation
     return
