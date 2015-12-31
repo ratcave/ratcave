4c263a4bd3dda93bb83d80535fcebcc5d252cd77
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2015-12-31 15:33:59 +0100

    removed devices.displays subpackage

--- ratcave/devices/__init__.py
+++ ratcave/devices/__init__.py
@@ -2,11 +2,6 @@ __author__ = 'ratcave'
 
 # Optional Imports
 try:
-    from .displays import propixx_utils
-except ImportError:
-    print("Warning: propixx_utils module not imported, as pypixxlib not found.")
-
-try:
     from .reward.ttl import USB_TTL
 except ImportError:
     print("Warning: USB_TTL device not imported.")
