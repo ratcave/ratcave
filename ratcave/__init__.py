27499a9d9b1ddbf5faa68156a917934c3176ffa3
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-02-10 18:18:21 +0100

    minor fix

--- ratcave/__init__.py
+++ ratcave/__init__.py
@@ -7,7 +7,7 @@ import pyglet
 pyglet.options['debug_gl'] = False
 
 from . import resources
-from ratcave.utils.shader import Shader
+from .shader import Shader
 from .mixins import Physical
 from .camera import Camera
 from .light import Light
