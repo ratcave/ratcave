c4296b2969f0af61e466193b8cbbddc39b81baa3
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-02-25 15:32:58 +0100

    tiny docs

--- ratcave/light.py
+++ ratcave/light.py
@@ -1,8 +1,7 @@
-__author__ = 'nicholas'
-
 from .camera import Camera
 
 class Light(Camera):
 
     def __init__(self, *args, **kwargs):
+        """Light class."""
         super(Light, self).__init__(*args, **kwargs)
