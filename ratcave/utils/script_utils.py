955e88c608381b47433fe4154349b0de734f9420
sirotalab <sirotalab>
2016-03-15 23:50:00 +0100

    removed a motivepy function that's only used for calibration scripts with Optitrack.  Moved it to another repository

--- ratcave/utils/script_utils.py
+++ ratcave/utils/script_utils.py
@@ -2,19 +2,7 @@ __author__ = 'ratcave'
 import numpy as np
 from . import orienting
 
-def motive_camera_vislight_configure():
-    import motive
-    for cam in motive.get_cams():
 
-            # All cameras should have frame rate changed.
-            cam.frame_rate = 30
-
-            if 'Prime 13' in cam.name:
-                cam.set_settings(video_mode=0, exposure=33000, threshold=80, intensity=0)  #check if 480 corresponds to these thousands described in motive
-                cam.image_gain = 8  # 8 is the maximum image gain setting
-                cam.set_filter_switch(False)
-            else:
-                cam.set_settings(0, cam.exposure, cam.threshold, cam.intensity)
 
 
 def correct_orientation_motivepy(rb, n_attempts=3):
