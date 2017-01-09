58531d17a7749cf84a2fe645fc55263de283373a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2017-01-09 14:42:45 +0100

    Moved temporary backwards compatibility methods to a new PhysicalComposite and PhysicalNode class, intended for Mesh, Camera, and Light classes, which provides a shortcut to the deeply-nested Mesh.obj.physical methods.  This can be a more temporary place for these shortcuts.

--- ratcave/physical.py
+++ ratcave/physical.py
@@ -1,3 +1,4 @@
+import abc
 import numpy as np
 import _transformations as trans
 from .utils import rotations as rotutils
@@ -17,55 +18,15 @@ class Physical(AutoRegisterObserver):
         """
         super(Physical, self).__init__(**kwargs)
 
-        self._rotation = rotutils.RotationEulerDegrees(*rotation)
-        self._position = rotutils.Translation(*position)
-        self._scale = rotutils.Scale(scale)
+        self.rotation = rotutils.RotationEulerDegrees(*rotation)
+        self.position = rotutils.Translation(*position)
+        self.scale = rotutils.Scale(scale)
 
         self.model_matrix = np.zeros((4, 4))
         self.normal_matrix = np.zeros((4, 4))
         self.view_matrix = np.zeros((4, 4))
 
         self.update()
-
-    @property
-    def position(self):
-        return self._position
-
-    @position.setter
-    def position(self, coords):
-        if isinstance(coords, rotutils.Translation):
-            self._position = coords
-        else:
-            self._position = rotutils.Translation(*coords)
-
-    @property
-    def rotation(self):
-        return self._rotation
-
-    @rotation.setter
-    def rotation(self, coords):
-        if isinstance(coords, rotutils.RotationBase):
-            self._rotation = coords
-        elif hasattr(coords, '__iter__'):
-            if len(coords) == 3:
-                self._rotation = rotutils.RotationEulerDegrees(*coords)
-            elif len(coords) == 4:
-                self._rotation = rotutils.RotationQuaternion(*coords)
-        else:
-            raise ValueError("rot must be xyz values, xyzw values, or inherit from RotationBase")
-
-    @property
-    def scale(self):
-        return self._scale
-
-    @scale.setter
-    def scale(self, coords):
-        if isinstance(coords, rotutils.Scale):
-            self._scale = coords
-        elif hasattr(coords, '__iter__'):
-            self._scale = rotutils.Scale(*coords)
-        else:
-            self._scale = rotutils.Scale(coords)
 
     def update(self):
         """Calculate model, normal, and view matrices from position, rotation, and scale data."""
@@ -76,65 +37,6 @@ class Physical(AutoRegisterObserver):
         self.view_matrix[:] = trans.inverse_matrix(self.model_matrix)
         self.model_matrix[:] = np.dot(self.model_matrix, self.scale.to_matrix())
         self.normal_matrix[:] = trans.inverse_matrix(self.model_matrix.T)
-
-    ##############################################################################################
-    ### Properties added to increase backwards compatibility.  Plan is to deprecate in future. ###
-    ##############################################################################################
-    @property
-    def x(self):
-        return self.position.x
-
-    @x.setter
-    def x(self, value):
-        self.position.x = value
-
-    @property
-    def y(self):
-        return self.position.y
-
-    @y.setter
-    def y(self, value):
-        self.position.y = value
-
-    @property
-    def z(self):
-        return self.position.z
-
-    @z.setter
-    def z(self, value):
-        self.position.z = value
-
-    @property
-    def rot_x(self):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        return self.rotation.x
-
-    @rot_x.setter
-    def rot_x(self, value):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        self.rotation.x = value
-
-    @property
-    def rot_y(self):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        return self.rotation.y
-
-    @rot_y.setter
-    def rot_y(self, value):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        self.rotation.y = value
-
-    @property
-    def rot_z(self):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        return self.rotation.z
-
-    @rot_z.setter
-    def rot_z(self, value):
-        assert isinstance(self.rotation, rotutils.RotationEulerDegrees)
-        self.rotation.z = value
-
-
 
 
 class PhysicalNode(Physical, SceneNode):
@@ -162,3 +64,106 @@ class PhysicalNode(Physical, SceneNode):
     @property
     def position_global(self):
         return tuple(self.model_matrix_global[:3, -1].tolist())
+
+
+class PhysicalCompositeBase(object):
+    """
+    An object that is composed of a Physical object, with the Physical object as an 'obj' attribute.
+    Provides shortcut properties to position, rotation, and scale methods to reduce apparant nesting in the interface.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def update(self):
+        self.obj.update()
+
+    @property
+    def position(self):
+        return self.obj.position.xyz
+
+    @position.setter
+    def position(self, value):
+        self.obj.position.xyz = value
+
+    @property
+    def x(self):
+        return self.obj.position.x
+
+    @x.setter
+    def x(self, value):
+        self.obj.position.x = value
+
+    @property
+    def y(self):
+        return self.obj.position.y
+
+    @y.setter
+    def y(self, value):
+        self.obj.position.y = value
+
+    @property
+    def z(self):
+        return self.obj.position.z
+
+    @z.setter
+    def z(self, value):
+        self.obj.position.z = value
+
+    @property
+    def rotation(self):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        return self.obj.rotation.xyz
+
+    @rotation.setter
+    def rotation(self, value):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        self.obj.rotation.xyz = value
+
+    @property
+    def rot_x(self):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        return self.obj.rotation.x
+
+    @rot_x.setter
+    def rot_x(self, value):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        self.obj.rotation.x = value
+
+    @property
+    def rot_y(self):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        return self.obj.rotation.y
+
+    @rot_y.setter
+    def rot_y(self, value):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        self.obj.rotation.y = value
+
+    @property
+    def rot_z(self):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        return self.obj.rotation.z
+
+    @rot_z.setter
+    def rot_z(self, value):
+        assert isinstance(self.obj.rotation, rotutils.RotationEulerDegrees)
+        self.obj.rotation.z = value
+
+    @property
+    def scale(self):
+        return self.obj.scale[0]
+
+    @scale.setter
+    def scale(self, value):
+        self.obj.scale[0] = value
+
+
+class PhysicalComposite(PhysicalCompositeBase):
+
+    def __init__(self, **kwargs):
+        self.obj = Physical(**kwargs)
+
+
+class PhysicalNodeComposite(PhysicalCompositeBase):
+
+    def __init__(self, **kwargs):
+        self.obj = PhysicalNode(**kwargs)
