077fc999dd8373526b4d921cbd58b28b517af276
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2017-01-12 21:48:18 +0100

    Removed shader from draw360 method, but didn't test.  Camera uniforms get sent slightly more often now, but the code is cleaned up.  Noticed a bug that will return an error related to the Camera.lens.aspect not being 1--need to fix this, but can't now, so I just left a todo.

--- ratcave/scene.py
+++ ratcave/scene.py
@@ -73,43 +73,34 @@ class Scene(Drawable):
                 mesh.draw(send_uniforms=send_mesh_uniforms)
 
 
-    def draw360_to_texture(self, cubetexture, shader=resources.genShader, autoclear=True, userdata={},
+    def draw360_to_texture(self, cubetexture, autoclear=True, userdata={},
              gl_states=(gl.GL_DEPTH_TEST, gl.GL_POINT_SMOOTH, gl.GL_TEXTURE_CUBE_MAP, gl.GL_TEXTURE_2D)):#, gl.GL_BLEND)):
         """
         Draw each visible mesh in the scene from the perspective of the scene's camera and lit by its light, and
         applies it to each face of cubetexture, which should be currently bound to an FBO.
         """
 
-        assert self.camera.aspect == 1. and self.camera.fov_y == 90
+        assert self.camera.lens.aspect == 1. and self.camera.lens.fov_y == 90  # todo: fix aspect property, which currently reads from viewport.
         assert type(cubetexture) == TextureCube, "Must render to TextureCube"
 
         # Enable 3D OpenGL states (glEnable, then later glDisable)
         with glutils.enable_states(gl_states):
             # gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 
-            # Bind Shader
-            with shader:
-
                 self.camera.update()
-
-                shader.uniformf('light_position', *self.light.position.xyz)
-                shader.uniform_matrixf('projection_matrix', self.camera.projection_matrix)
-                shader.uniformf('camera_position', *self.camera.position.xyz)
+                self.light.update()
+                self.uniforms.send()
 
                 # Pre-Calculate all 6 view matrices
-                view_matrices = []
-                view_matrix_loc = shader.get_uniform_location('view_matrix')
-                for rotation in [[180, 90, 0], [180, -90, 0], [90, 0, 0], [-90, 0, 0], [180, 0, 0], [0, 0, 180]]:
-                    self.camera.rotation[:] = rotation
-                    self.camera.update_view_matrix()
-                    view_matrices.append(self.camera.view_matrix)
-
                 for mesh_idx, mesh in enumerate(self.root):
-                    for face, view_matrix in enumerate(view_matrices):
+                    for face, rotation in enumerate([[180, 90, 0], [180, -90, 0], [90, 0, 0], [-90, 0, 0], [180, 0, 0], [0, 0, 180]]):
+                        self.camera.rotation.xyz = rotation
+                        self.camera.update()
+                        self.uniforms.send()
+
                         cubetexture.attach_to_fbo(face)
                         if autoclear and not mesh_idx:
                             self.clear()
-                        shader.uniform_matrixf('view_matrix', view_matrix, loc=view_matrix_loc)
-                        mesh.draw(shader=shader, send_uniforms=not face)
+                        mesh.draw(send_uniforms=not face)
 
 
