21d72d5dd8826535fd2b83acd5d63f7a4251b5c7
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2017-02-16 10:55:59 +0100

    texture auto-applied to mesh if map_kD is defined in a .mtl file

--- ratcave/wavefront.py
+++ ratcave/wavefront.py
@@ -1,7 +1,7 @@
 from .mesh import Mesh
 from six import iteritems
 from wavefront_reader import read_wavefront
-
+from . import Texture
 
 class WavefrontReader(object):
 
@@ -12,13 +12,13 @@ class WavefrontReader(object):
                              'Ni': 'Ni',
                              'Ns': 'spec_weight',
                              'd': 'd',
-                             'illum': 'illum'
+                             'illum': 'illum',
+                             'map_Kd': 'map_Kd',
                              }
 
     def __init__(self, file_name):
         """
         Reads Wavefront (.obj) files created in Blender to build ratCAVE.graphics Mesh objects.
-
         :param file_name: .obj file to read (assumes an accompanying .mtl file has the same base file name.)
         :type file_name: str
         :return:
@@ -37,17 +37,19 @@ class WavefrontReader(object):
         mesh = Mesh.from_incomplete_data(name=name, vertices=vertices, normals=normals, texcoords=texcoords, **kwargs)
 
         uniforms = kwargs['uniforms'] if 'uniforms' in kwargs else {}
-        if 'material' in body:
-            material_props = {self.material_property_map[key]: value for key, value in iteritems(body['material'])}
-            for key, value in iteritems(material_props):
-                if hasattr(value, '__len__'):  # iterable materials
-                    mesh.uniforms[key] = value
-                elif key in ['d', 'illum']:  # integer materials
-                    mesh.uniforms[key] = value
-                elif key in ['spec_weight', 'Ni']:  # float materials: should be specially converted to float if not already done.
-                    mesh.uniforms[key] = float(value)
-                elif isinstance(value, str):
-                    setattr(mesh, key, value)
+        material_props = {self.material_property_map[key]: value for key, value in iteritems(body['material'])}
+        for key, value in iteritems(material_props):
+            if isinstance(value, str):
+                if key == 'map_Kd':
+                    mesh.texture = Texture.from_image(value)
                 else:
-                    print('Warning: Not applying uniform {}: {}'.format(key, value))
+                    setattr(mesh, key, value)
+            elif hasattr(value, '__len__'):  # iterable materials
+                mesh.uniforms[key] = value
+            elif key in ['d', 'illum']:  # integer materials
+                mesh.uniforms[key] = value
+            elif key in ['spec_weight', 'Ni']:  # float materials: should be specially converted to float if not already done.
+                mesh.uniforms[key] = float(value)
+            else:
+                print('Warning: Not applying uniform {}: {}'.format(key, value))
         return mesh
