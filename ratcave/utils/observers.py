c1927dbab91feb8cbfeff4e76441dee889e9f4db
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-01-04 15:18:16 +0100

    Removed unused observable->observer signal.

--- ratcave/utils/observers.py
+++ ratcave/utils/observers.py
@@ -10,14 +10,14 @@ class Observable(object):
         if not issubclass(observer.__class__, Observer):
             raise TypeError()
         self._observers.add(observer)
-        observer.notify(self)
+        observer.notify()
 
     def unregister_observer(self, observer):
         self._observers.remove(observer)
 
     def notify_observers(self):
         for observer in self._observers:
-            observer.notify(self)
+            observer.notify()
 
 
 class IterObservable(Observable):
@@ -32,12 +32,10 @@ class Observer(object):
     def __init__(self, **kwargs):
         super(Observer, self).__init__(**kwargs)
         self._requires_update = True
-        self._changed_observables = []
 
-    def notify(self, observable):
+    def notify(self):
         """Flags Observer to perform update() at proper time."""
         self._requires_update = True
-        self._changed_observables.append(observable)
 
     def on_change(self):
         """Callback for if change  detected. Meant to be overridable by subclasses."""
@@ -45,14 +43,13 @@ class Observer(object):
 
     def update(self):
         """Check if any updates happened. If not, return False.  Else, perform callback and reset update flag, and return True."""
-        assert not hasattr(super, 'update')
-        if not self._requires_update:
-            return False
-        else:
+        if self._requires_update:
             self.on_change()
             self._requires_update = False
-            self._changed_observables = []
             return True
+        else:
+            return False
+
 
 
 class AutoRegisterObserver(Observer):
