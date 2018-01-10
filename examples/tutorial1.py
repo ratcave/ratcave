8d49fe629567413565515cc863a4301b8004f3ec
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-01-10 16:56:37 +0100

    Added tests of the observer.py module.

--- examples/tutorial1.py
+++ examples/tutorial1.py
@@ -2,6 +2,7 @@ import pyglet
 import ratcave as rc
 import itertools as it
 import numpy as np
+from contextlib import ExitStack
 
 # Create Window
 window = pyglet.window.Window(resizable=True, vsync=False)
@@ -37,28 +38,26 @@ monkey.textures.append(fbo_shadow.texture)
 @window.event
 def on_draw():
     window.clear()
-    with rc.resources.shadow_shader, fbo_shadow, rc.default_states, light:
-        window.clear()
-        for x, y in it.product([-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2]):
-            monkey.position.x = x
-            monkey.position.y = y
-            monkey.drawmode = rc.POINTS if x % 2 and y % 2 else rc.TRIANGLES
-            monkey.scale.z = np.linalg.norm((x, y)) / 10. + .03
-            monkey.draw()
+    with ExitStack() as stack:
+        for shader in [rc.resources.shadow_shader, rc.default_shader]:
+            with shader, rc.default_states, light, rc.default_camera:
+                if shader == rc.resources.shadow_shader:
+                    stack.enter_context(fbo_shadow)
+                    window.clear()
+                else:
+                    stack.close()
 
-    with rc.default_shader, rc.default_states, light, rc.default_camera:
-        for x, y in it.product([-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2]):
-            monkey.position.x = x
-            monkey.position.y = y
+                for x, y in it.product([-2, -1, 0, 1, 2], [-2, -1, 0, 1, 2]):
+                    monkey.position.x = x
+                    monkey.position.y = y
+                    monkey.drawmode = rc.POINTS if x % 2 and y % 2 else rc.TRIANGLES
+                    monkey.uniforms['diffuse'][0] = (x + 1) / 4.
+                    monkey.uniforms['diffuse'][1:] = (y + 1) / 4.
+                    monkey.scale.z = np.linalg.norm((x, y)) / 10. + .03
+                    monkey.draw()
 
-            monkey.drawmode = rc.POINTS if x % 2 and y % 2 else rc.TRIANGLES
-            monkey.uniforms['diffuse'][0] = (x + 1) / 4.
-            monkey.uniforms['diffuse'][1:] = (y + 1) / 4.
-            monkey.scale.z = np.linalg.norm((x, y)) / 10. + .03
+                plane.draw()
 
-
-            monkey.draw()
-        plane.draw()
     fps_display.draw()
 
 def update(dt):
---
+++ tests/test_observer.py
@@ -0,0 +1,97 @@
+from __future__ import print_function
+import unittest
+from ratcave.utils import Observable, Observer, AutoRegisterObserver, IterObservable
+import numpy as np
+
+
+np.set_printoptions(suppress=True, precision=2)
+
+class TestObserver(unittest.TestCase):
+    """
+    Run tests from main project folder.
+    """
+
+
+    def test_observer_receives_observable_signals(self):
+
+        obs = Observer()
+        obs2 = Observer()
+        able = Observable()
+        able.register_observer(obs)
+
+        self.assertIn(obs, able._observers)
+        self.assertNotIn(obs2, able._observers)
+
+    def test_observer_executes_when_notified(self):
+
+        class DummyObserver(Observer):
+            def on_change(self):
+                self.test_value += 1
+
+        obs = DummyObserver()
+        obs.test_value = 1
+
+        obs2 = DummyObserver()
+        obs2.test_value = 1
+
+        self.assertEqual(obs.test_value, 1)
+        obs.notify()
+        self.assertEqual(obs.test_value, 2)
+        obs.notify()
+        self.assertEqual(obs.test_value, 3)
+        self.assertEqual(obs2.test_value, 1)
+
+
+        able = Observable()
+        able.register_observer(obs)
+        self.assertEqual(obs.test_value, 4)
+
+        able.notify_observers()
+        self.assertEqual(obs.test_value, 5)
+        self.assertEqual(obs2.test_value, 1)
+
+    def test_observer_executes_for_every_single_notification(self):
+        class DummyObserver(Observer):
+            def on_change(self):
+                self.test_value += 1
+
+        obs = DummyObserver()
+        obs.test_value = 0
+
+        ables = [Observable() for el in range(5)]
+        for idx, able in enumerate(ables):
+            able.register_observer(obs)
+            self.assertEqual(obs.test_value, idx + 1)
+
+        for idx, able in enumerate(ables):
+            able.notify_observers()
+            self.assertEqual(obs.test_value, idx + len(ables) + 1)
+
+    def test_observable_keeps_no_duplicates(self):
+        obs = Observer()
+        able = Observable()
+        self.assertEqual(len(able._observers), 0)
+        able.register_observer(obs)
+        self.assertEqual(len(able._observers), 1)
+        able.register_observer(obs)
+        self.assertEqual(len(able._observers), 1)
+        able.register_observer(Observer())
+        self.assertEqual(len(able._observers), 2)
+
+        able.unregister_observer(obs)
+        self.assertEqual(len(able._observers), 1)
+
+    def test_autoregistration(self):
+        obs = Observer()
+        able = Observable()
+        self.assertEqual(len(able._observers), 0)
+        obs.attr1 = able
+        self.assertEqual(len(able._observers), 0)
+
+        autoobs = AutoRegisterObserver()
+        autoobs.attr1 = able
+        self.assertEqual(len(able._observers), 1)
+        autoobs.attr1 = able
+        self.assertEqual(len(able._observers), 1)
+        autoobs.attr2 = able
+        self.assertEqual(len(able._observers), 1)
