277abaca2c9ec8c54fa7838095f0e662e5d4d9dc
nickdg <delgrosso@bio.lmu.de>
2018-01-12 10:57:42 +0100

    Renamed and fixed demo.

--- examples/structure_from_motion_psychopy_demo.py
+++ examples/structure_from_motion_psychopy_demo.py
@@ -3,7 +3,7 @@ import ratcave as rc
 import numpy as np
 from numpy.random import random
 
-n_points = 200
+n_points = 1000
 width, height = 0.2, 0.5
 theta = random(n_points) * np.pi * 2
 verts = np.vstack((np.sin(theta) * width, (random(n_points) - .5) * height, np.cos(theta) * width)).T
@@ -13,14 +13,13 @@ cylinder.uniforms['diffuse'] = 1., 1., 1.
 cylinder.uniforms['flat_shading'] = True
 
 scene = rc.Scene(meshes=[cylinder], bgColor=(0., 0, 0))
-shader = rc.Shader.from_file(*rc.resources.genShader)
 scene.camera.projection = rc.OrthoProjection()
 
 win = visual.Window()
 
 while 'escape' not in event.getKeys():
-    cylinder.rotation.y += 100 * .02
-    with shader:
+    cylinder.rotation.y += .02
+    with rc.default_shader:
         scene.draw()
     win.flip()
 
