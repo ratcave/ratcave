0d3b275d37f5d7f2598c07df3d727b9c518d8cbc
nickdg <delgrosso@bio.lmu.de>
2018-01-12 11:03:44 +0100

    some example directory cleanup

--- examples/shaders/gabor.frag.glsl
+++ examples/shaders/gabor.frag.glsl
@@ -10,6 +10,5 @@
  {
     brightness = sin(gl_FragCoord.x / width) / 2. + .5;
     brightness *= (1. / (50. * sqrt(2 * pi))) * exp(-.5 * pow(((gl_FragCoord.x - 300.) / 50.), 2));
-    final_color = vec4(vec3(brightness), 1.) ;
-    //final_color = vec4(1., 0., 0., 1.);
+    final_color = vec4(vec3(brightness), 1.);
  }
