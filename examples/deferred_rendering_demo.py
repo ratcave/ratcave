94a188546fae02e71d1657e2608a871d7a5f5d54
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-01-18 14:42:20 +0100

    Made monkey-starfield.

--- examples/deferred_rendering_demo.py
+++ examples/deferred_rendering_demo.py
@@ -2,22 +2,29 @@ import pyglet
 import ratcave as rc
 
 win = pyglet.window.Window(resizable=True)
-pyglet.clock.schedule(lambda dt: dt)
 
 reader = rc.WavefrontReader(rc.resources.obj_primitives)
 stars = reader.get_mesh('Torus', drawmode=rc.POINTS)
-stars.point_size = .4
+stars.point_size = 3
 stars.uniforms['diffuse'] = 1., 1., 1.
 stars.position.xyz = 0, 0, -2
-stars.scale.xyz = .5
+stars.scale.xyz = 1.2
 
 monkey = reader.get_mesh('Monkey', position=(0, 0, -2))
-fbo = rc.FBO(texture=rc.Texture)
+monkey.uniforms['flat_shading'] = True
+
+fbo = rc.FBO(texture=rc.Texture(width=win.width, height=win.height))
+
+def update(dt):
+    stars.rotation.z += 10. * dt
+pyglet.clock.schedule(update)
+
 
 @win.event
 def on_draw():
     with rc.default_shader:
         with fbo:
+            win.clear()
             stars.draw()
         with fbo.texture:
             monkey.draw()
