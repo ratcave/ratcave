144d05763d447789e436c28dc72d73fc53d025a2
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2018-02-16 14:02:37 +0100

    updated demos

--- examples/dotstim_demo.py
+++ examples/dotstim_demo.py
@@ -3,12 +3,13 @@ import ratcave as rc
 import numpy as np
 from numpy.random import random
 
-n_points = 200
+n_points = 2000
 width, height = 0.2, 0.5
 theta = random(n_points) * np.pi * 2
 verts = np.vstack((np.sin(theta) * width, (random(n_points) - .5) * height, np.cos(theta) * width)).T
 
-cylinder = rc.Mesh.from_incomplete_data(verts, drawmode=rc.Mesh.points, position=(0, 0, -2), point_size=2, dynamic=True,
+cylinder = rc.Mesh.from_incomplete_data(verts, drawmode=rc.Mesh.points, position=(0, 0, -2),
+                                        point_size=1, dynamic=True,
                                         mean_center=False)
 cylinder.uniforms['diffuse'] = 1., 1., 1.
 cylinder.uniforms['flat_shading'] = True
@@ -23,18 +24,10 @@ def on_draw():
         scene.draw()
 
 
-
-turn = 0
 def update(dt):
-    global turn
-    turn += 1
-    if not turn % 1:
-        theta = random(n_points) * np.pi * 2
-        verts = np.vstack((np.sin(theta) * width, (random(n_points) - .5) * height, np.cos(theta) * width)).T
-        cylinder.vertices = verts
-
-    # print(cylinder.arrays[0][0, :])
-    # cylinder.rotation.z += 100 * dt
-pyglet.clock.schedule(update)
+    theta = random(n_points) * np.pi * 2
+    verts = np.vstack((np.sin(theta) * width, (random(n_points) - .5) * height, np.cos(theta) * width)).T
+    cylinder.vertices = verts
+pyglet.clock.schedule_interval(update, 1/60.)
 
 pyglet.app.run()
