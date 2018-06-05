98c20ed707504565d17e00fe50de0f4f8582552c
Nicholas A. Del Grosso <delgrosso@bio.lmu.de>
2018-06-05 14:58:34 +0200

    all conditional on appveyor
    textures need opengl, and appveyor isn't supporting the version we need, so appveyor should skip the texture tests until we can get a windows testing environment that supports this.

--- tests/test_texture.py
+++ tests/test_texture.py
@@ -1,56 +1,58 @@
 from ratcave import texture
 import pytest
+import os
 
-@pytest.fixture
-def tex():
-    return texture.Texture()
+if not 'APPVEYOR' in os.environ:
+    @pytest.fixture
+    def tex():
+        return texture.Texture()
 
-@pytest.fixture
-def cubetex():
-    return texture.TextureCube()
+    @pytest.fixture
+    def cubetex():
+        return texture.TextureCube()
 
-@pytest.fixture
-def depthtex():
-    return texture.DepthTexture()
+    @pytest.fixture
+    def depthtex():
+        return texture.DepthTexture()
 
-def test_texture_attributes_created():
-    old_id = 0
-    for idx, (w, h) in enumerate([(1024, 1024), (256, 128), (200, 301)]):
-        tex = texture.Texture(width=w, height=h)
-        assert tex.width == w
-        assert tex.height == h
-        assert tex.id != old_id
-        old_id = tex.id
+    def test_texture_attributes_created():
+        old_id = 0
+        for idx, (w, h) in enumerate([(1024, 1024), (256, 128), (200, 301)]):
+            tex = texture.Texture(width=w, height=h)
+            assert tex.width == w
+            assert tex.height == h
+            assert tex.id != old_id
+            old_id = tex.id
 
-    cube = texture.TextureCube(width=1024, height=1024)
-    assert cube.width == 1024
-    assert cube.height == 1024
-    with pytest.raises(ValueError):
-        cube = texture.TextureCube(width=400, height=600)
+        cube = texture.TextureCube(width=1024, height=1024)
+        assert cube.width == 1024
+        assert cube.height == 1024
+        with pytest.raises(ValueError):
+            cube = texture.TextureCube(width=400, height=600)
 
 
-def test_texture_default_uniform_names(tex, cubetex, depthtex):
-    assert 'TextureMap' in tex.uniforms
-    assert 'TextureMap_isBound' in tex.uniforms
-    assert 'CubeMap' in cubetex.uniforms
-    assert 'CubeMap_isBound' in cubetex.uniforms
-    assert 'DepthMap' in depthtex.uniforms
-    assert 'DepthMap_isBound' in depthtex.uniforms
-    assert 'CubeMap' not in tex.uniforms
-    assert 'CubeMap_isBound' not in tex.uniforms
-    assert 'TextureMap' not in cubetex.uniforms
+    def test_texture_default_uniform_names(tex, cubetex, depthtex):
+        assert 'TextureMap' in tex.uniforms
+        assert 'TextureMap_isBound' in tex.uniforms
+        assert 'CubeMap' in cubetex.uniforms
+        assert 'CubeMap_isBound' in cubetex.uniforms
+        assert 'DepthMap' in depthtex.uniforms
+        assert 'DepthMap_isBound' in depthtex.uniforms
+        assert 'CubeMap' not in tex.uniforms
+        assert 'CubeMap_isBound' not in tex.uniforms
+        assert 'TextureMap' not in cubetex.uniforms
 
-    newtex = texture.Texture(name='NewMap')
-    assert newtex.name == 'NewMap'
-    assert 'NewMap' in newtex.uniforms
-    assert 'NewMap_isBound' in newtex.uniforms
-    assert 'TextureMap' not in newtex.uniforms
+        newtex = texture.Texture(name='NewMap')
+        assert newtex.name == 'NewMap'
+        assert 'NewMap' in newtex.uniforms
+        assert 'NewMap_isBound' in newtex.uniforms
+        assert 'TextureMap' not in newtex.uniforms
 
-    newtex.name = 'Changed'
-    assert newtex.name == 'Changed'
-    assert 'Changed' in newtex.uniforms
-    assert 'Changed_isBound' in newtex.uniforms
-    assert 'NewMap' not in newtex.uniforms
-    assert 'NewMap_isBound' not in newtex.uniforms
+        newtex.name = 'Changed'
+        assert newtex.name == 'Changed'
+        assert 'Changed' in newtex.uniforms
+        assert 'Changed_isBound' in newtex.uniforms
+        assert 'NewMap' not in newtex.uniforms
+        assert 'NewMap_isBound' not in newtex.uniforms
 
 
