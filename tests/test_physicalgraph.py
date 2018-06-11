61ea063eabf8f20acea4b2534dc9775f2efe831f
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-06-12 00:04:18 +0200

    test physcialgraph fixed, scale can not be equal to zero

--- tests/test_physicalgraph.py
+++ tests/test_physicalgraph.py
@@ -8,12 +8,12 @@ from _transformations import inverse_matrix as inv
 np.random.seed(100)
 np.set_printoptions(suppress=True, precision=5)
 
-class TestPhysical(unittest.TestCase):
+class TestPhysicalGraph(unittest.TestCase):
     """
     Run tests from main project folder.
     """
-    def test_child_update_matrix_function(self):
-        for i in range(100):
+    def test_child_update_model_matrix_transform_function(self):
+        for _ in range(200):
             child =  PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3))
             parent = PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3))
             new_mm = np.dot(inv(parent.model_matrix), child.model_matrix)
@@ -40,16 +40,16 @@ class TestPhysical(unittest.TestCase):
             old_mm = child.model_matrix_global.copy()
             parent.add_child(child, modify=True)
 
-            self.assertTrue(np.isclose(old_mm, child.model_matrix_global,atol=1.e-3).all())  # TODO: Improve numerical accuracy of parenting
+            self.assertTrue(np.isclose(old_mm, child.model_matrix_global, atol=1.e-4).all())  # TODO: Improve numerical accuracy of parenting
 
 
     def test_add_child_connection_modify_scale(self):
         # N = P * O
         for _ in range(200):
-            child =  PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3), scale=np.random.randint(0, 360))
-            parent = PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3), scale=np.random.randint(0, 360))
+            child =  PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3), scale=np.random.randint(1, 360))
+            parent = PhysicalGraph(position=np.random.randint(0, 360, 3), rotation=np.random.randint(0, 360, 3), scale=np.random.randint(1, 360))
 
             old_mm = child.model_matrix_global.copy()
             parent.add_child(child, modify=True)
 
-            self.assertTrue(np.isclose(old_mm, child.model_matrix_global,atol=1.e-3).all())  # TODO: Improve numerical accuracy of parenting
+            self.assertTrue(np.isclose(old_mm, child.model_matrix_global,atol=1.e-4).all())  # TODO: Improve numerical accuracy of parenting
